4c4055abbd2adcf15580431fe7c398e2bc390d36
league <league@localhost>
2026-01-02 18:25:14 -0800

    effawfeaf

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -1,5 +1,9 @@
 package _01_methods._5_FlappyBird;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import processing.core.PApplet;
 
 public class FlappyBird extends PApplet {
@@ -13,6 +17,9 @@ public class FlappyBird extends PApplet {
     int upperPipeHeight = (int) random(100, 400);
     int pipeGap = 100;
     int lowerY = upperPipeHeight + pipeGap;
+    int pipeWidth = 80;
+    Random random = new Random();
+    int something = random.nextInt(7);
 
     @Override
     public void settings() {
@@ -33,20 +40,50 @@ public class FlappyBird extends PApplet {
     	birdY+=birdYVelocity;
     	birdYVelocity+=gravity;
     	fill(30, 240, 56);
-    	rect(pipeX, 0, 80, upperPipeHeight);
-    	rect(pipeX, lowerY, 80, 700);
+    	rect(pipeX, 0, pipeWidth, upperPipeHeight);
+    	rect(pipeX, lowerY, pipeWidth, 700);
     	pipeX-=8;
     	teleportPipes();
-    	
-    }
+    	if(intersectsPipes() == true) {
+    		int retry = JOptionPane.showOptionDialog(null, "u died", "", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String [] {"revive :)", "ragequit", "..."}, null);
+    		if(retry == 0) {
+    			JOptionPane.showMessageDialog(null, "HA! bro really thought");
+    			System.exit(0);
+    		}
+    		else if(retry == 2) {	
+    			birdX = 60;
+    		    birdY = 300;
+    		    birdYVelocity = -10;
+    		    gravity = 1;
+    		    pipeX = 700;
+    		    upperPipeHeight = (int) random(100, 400);
+    		    pipeGap = 100;
+    		    lowerY = upperPipeHeight + pipeGap;
+    		    pipeWidth = 80;
+    		}
+    		else if(retry == 1) {
+        		System.exit(0);
+        	}
+    	}
+    		
+    	}
+    
     
     public void teleportPipes() {
-    	if(pipeX <= -80) {
+    	if(pipeX <= -pipeWidth) {
     		pipeX = 800;
     		upperPipeHeight = (int) random(100, 400);
     		lowerY = upperPipeHeight + pipeGap;
     	}
     }
+    
+    boolean intersectsPipes() { 
+        if (birdY < upperPipeHeight && birdX > pipeX && birdX < (pipeX+pipeWidth)){
+           return true; }
+       else if (birdY>lowerY && birdX > pipeX && birdX < (pipeX+pipeWidth)) {
+           return true; }
+       else { return false; }
+}
     
     public void mousePressed() {
     	if(mousePressed) {
