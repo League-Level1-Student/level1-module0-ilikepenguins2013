5f1bfda5001ab6b0a67f35e7ec11bb36a9fb7b07
league <league@localhost>
2026-01-02 17:58:18 -0800

    added pipes, killing function coming soon '

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -9,6 +9,10 @@ public class FlappyBird extends PApplet {
     int birdY = 300;
     int birdYVelocity = -10;
     int gravity = 1;
+    int pipeX = 700;
+    int upperPipeHeight = (int) random(100, 400);
+    int pipeGap = 100;
+    int lowerY = upperPipeHeight + pipeGap;
 
     @Override
     public void settings() {
@@ -17,20 +21,31 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void setup() {
-        
+    	
     }
 
     @Override
     public void draw() {
     	background(135, 206, 235);
+    	noStroke();
     	fill(72, 62, 73);
-    	stroke(72, 62, 73);
     	ellipse(birdX, birdY, 45, 45);
     	birdY+=birdYVelocity;
     	birdYVelocity+=gravity;
+    	fill(30, 240, 56);
+    	rect(pipeX, 0, 80, upperPipeHeight);
+    	rect(pipeX, lowerY, 80, 700);
+    	pipeX-=8;
+    	teleportPipes();
     	
-    	
-    	
+    }
+    
+    public void teleportPipes() {
+    	if(pipeX <= -80) {
+    		pipeX = 800;
+    		upperPipeHeight = (int) random(100, 400);
+    		lowerY = upperPipeHeight + pipeGap;
+    	}
     }
     
     public void mousePressed() {
